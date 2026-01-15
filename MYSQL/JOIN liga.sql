28892fea36660d976d2d2327340d66e8e05eb026
dav0009-byte <dav0009@alu.medac.es>
2026-01-15 12:57:23 +0100

    STATIC

---
+++ MYSQL/JOIN liga.sql
@@ -0,0 +1,43 @@
+use liga; 
+
+select * from equipo;
+select * from jugador; 
+select * from partido; 
+
+-- 1. Datos del jugador más alto en la liga 
+
+select * from jugador where altura = ( select max(altura) from jugador);
+
+
+-- 2. Datos de todos los jugadores que pertenecen a la caja laboral
+
+select j.* from jugador j join equipo e on j.equipo=e.id_equipo where e.nombre = "Caja Laboral";
+
+-- 3. Suma de las alturas de los jugadores que sean del CAI Zaragoza o del Real Madrid 
+
+select sum(altura) from jugador j join equipo e on j.equipo=e.id_equipo where e.nombre = "Real Madrid" or e.nombre = "CAI Zaragoza";
+
+-- 4. Datos de los jugadores que miden más que todos los jugadores de la Caja Laboral 
+
+select * from jugador where altura > (
+select max(altura) from jugador j join equipo e on j.equipo = e.id_equipo where e.nombre = "Caja Laboral");
+
+-- 5. Datos de los jugadores mejor y peor pagado de la liga 
+
+select * from jugador where salario = (
+select max(salario) from jugador) or salario = ( select min(salario) from jugador); 
+
+-- 6. Datos Datos del jugador más antiguo, es decir, el que lleva más tiempo dado de alta en un equipo.
+
+select * from jugador where fecha_alta = (select max(fecha_alta) from jugador);
+
+-- 7. Datos de los equipos que tienen más de tres jugadores registrados.
+
+select * from equipos where equipos = ( select * from equipos e join    );
+
+-- 8. Mostrar el nombre del jugador, el nombre del equipo al que pertenece y su posición.
+
+-- 9. Mostrar el nombre de cada equipo y el nombre de su capitán o capitanes.
+
+-- 10. Datos de los equipos y el número de partidos que han jugado como locales.
+
